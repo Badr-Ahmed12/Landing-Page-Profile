48d3fa282d995048d5d9eb64324f9da2a138ec86
Badr DeLL Adhm <isob83709@gmail.com>
2025-01-17 01:41:46 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,26 +10,35 @@ export const metadata: Metadata = {
     title: "Visual Designer | Portfolio & Blog",
     description:
       "Experience in designing for web and mobile. Check out my portfolio and blog for more information.",
-    image: "/og-image.png",
+    images: [
+      {
+        url: "/og-image.png",
+        alt: "Visual Designer Portfolio Image",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Visual Designer | Portfolio & Blog",
     description:
       "Experience in designing for web and mobile. Check out my portfolio and blog for more information.",
-    image: "/og-image.png",
+    images: [
+      {
+        url: "/og-image.png",
+        alt: "Visual Designer Portfolio Image",
+      },
+    ],
   },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className="bg-white">{children}</body>
     </html>
   );
 }
-
