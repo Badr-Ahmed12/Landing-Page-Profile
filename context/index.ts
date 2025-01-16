08e712213eaec8f35a731d5d6d8a3e60f260f92b
Badr DeLL Adhm <isob83709@gmail.com>
2025-01-17 01:49:49 +0200

    first commit

--- context/index.ts
+++ context/index.ts
@@ -1,16 +1,16 @@
-import companyLogos from "@/public/Group 5334.png";
-import companyLogosTraced1 from "@/public/image 1 (Traced).png";
-import companyLogosTraced2 from "@/public/image 2 (Traced).png";
-import companyLogosTraced3 from "@/public/image 5 (Traced).png";
-import img from "@/public/HeaderImage 1.png";
-import img1 from "@/public/Image (1).png";
-import img2 from "@/public/Image (2).png";
-import img3 from "@/public/Image (3).png";
-import img4 from "@/public/Image (4).png";
-import img5 from "@/public/Image (5).png";
-import img6 from "@/public/Image (6).png";
-import img7 from "@/public/Client Image.png";
-import { Logo, ContactInfo, Project, HeroText, NavLink, Skill, Testimonial } from "./types/index.d.ts";
+import companyLogos from "@/public/Group 5334.png?trace";
+import companyLogosTraced1 from "@/public/image 1 (Traced).png?trace";
+import companyLogosTraced2 from "@/public/image 2 (Traced).png?trace";
+import companyLogosTraced3 from "@/public/image 5 (Traced).png?trace";
+import img from "@/public/HeaderImage 1.png?trace";
+import img1 from "@/public/Image (1).png?trace";
+import img2 from "@/public/Image (2).png?trace";
+import img3 from "@/public/Image (3).png?trace";
+import img4 from "@/public/Image (4).png?trace";
+import img5 from "@/public/Image (5).png?trace";
+import img6 from "@/public/Image (6).png?trace";
+import img7 from "@/public/Client Image.png?trace";
+import { Logo, ContactInfo, Project, HeroText, NavLink, Skill, Testimonial } from "@/types/index.d.ts";
 
 export const Logos: Logo[] = [
   { src: companyLogos, alt: "Company Logos" },
@@ -49,7 +49,7 @@ export const skills: Skill[] = [
   { title: "Product Design", description: "This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com", color: "bg-red-200" },
   { title: "Visual Design", description: "This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com", color: "bg-red-400" },
   { title: "Art Direction", description: "This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com", color: "bg-green-500" },
-];
+};
 
 export const testimonials: Testimonial[] = [
   { text: "This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com", name: "Gemma Nolen", role: "Google", image: img7, rating: 5 },
@@ -59,3 +59,4 @@ export const testimonials: Testimonial[] = [
   { text: "This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com", name: "Gina Smith", role: "Amazon", image: img7, rating: 5 },
   { text: "This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com", name: "Jim Doe", role: "Tesla", image: img7, rating: 4 },
 ];
+
